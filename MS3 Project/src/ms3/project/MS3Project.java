222acf2edc377388a4e0f5dd30e588c42129ff2d
TWJones01 <TWJones01@10.103.16.91>
2020-05-29 15:08:45 -0400

    Test Commit.

---
+++ MS3 Project/src/ms3/project/MS3Project.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ms3.project;
+
+/**
+ *
+ * @author TWJones01
+ */
+public class MS3Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+    }
+    
+}
